2e151fa2846f207a3c90a9d8db34cd77d6de623e
tsunglintsai <tsunglin.tsai@gmail.com>
2013-02-08 17:01:10 -0800

    Updated domain class interfaces

--- CardGame/SetGameCard.h
+++ CardGame/SetGameCard.h
@@ -9,18 +9,82 @@
 #import "Card.h"
 
 /**
- * A SetGameCard object is varying in four features: number (one, two, or three); symbol (diamond, squiggle, oval); shading (solid, striped, or open); and color (red, green, or purple).
+ * A SetGameCard object is varying in four features: number (1, 2, or 3); symbol (diamond, squiggle, oval); shading (solid, striped, or open); and color (red, green, or purple).
  *
+ *  - `number` : { 1 | 2 | 3 }
+ *  - `symbol` : { diamond | squiggle | oval }
+ *  - `shading` : { solid | striped | open }
+ *  - `color` : { red | green | purple }
+ *  @warning *Note:* Any value can passed into attributed. However, class will ignore invalid values. reccomend use convenience initializer to create SetGameCard object;
  **/
 @interface SetGameCard : Card
 
+///---------------------------------------------------------------------------------------
+/// @name Convenience Initializer
+///---------------------------------------------------------------------------------------
+
+/**
+ * Returns a newly initialized SetGameCard object with value specified.
+ **/
+- (id)initWithNumber:(NSUInteger)number withSymbol:(NSString*)symbol withShading:(NSString*)shading withColor:(NSString*)color;
+
+///---------------------------------------------------------------------------------------
+/// @name SetGameCard Attributes
+///---------------------------------------------------------------------------------------
+
+/**
+ * The value of number.
+ **/
+@property (nonatomic) NSUInteger number;
+/**
+ * The value of symbol.
+ **/
+@property (strong,nonatomic) NSString *symbol;
+/**
+ * The value of shading.
+ **/
+@property (strong,nonatomic) NSString *shading;
+/**
+ * The value of color.
+ **/
+@property (strong,nonatomic) NSString *color;
 
 ///---------------------------------------------------------------------------------------
 /// @name Get Card Content
 ///---------------------------------------------------------------------------------------
 
 /**
- * The attributed string content of card.
+ * Gets the attributed string content of card.
+ * @return attributed string content.
  **/
 -(NSAttributedString*) getContentAsAttributedString;
+
+///---------------------------------------------------------------------------------------
+/// @name Get Valid Values
+///---------------------------------------------------------------------------------------
+
+/**
+ * Gets the number of SetGameCard object
+ * @return array of NSNumber .
+ **/
++(NSArray*) validNumbers;
+
+/**
+ * Gets the symbol of SetGameCard object
+ * @return array of NSString.
+ **/
++(NSArray*) validSymbol;
+
+/**
+ * Gets the shading of SetGameCard object
+ * @return array of NSString.
+ **/
++(NSArray*) validShading;
+
+/**
+ * Gets the color of SetGameCard object
+ * @return array of NSString.
+ **/
++(NSArray*) validColor;
+
 @end
--- CardGame/SetGameDeck.m
+++ CardGame/SetGameDeck.m
@@ -11,4 +11,25 @@
 
 @implementation SetGameDeck
 
+/**
+ *
+ **/
+-(SetGameDeck*) init{
+    /*
+    self = [super init];
+    if(self){
+        for(NSString *suit in [PlayingCard validSuits]){
+            for(NSInteger rank = 1 ; rank <= [PlayingCard maxRank]; rank++){
+                PlayingCard *card = [[PlayingCard alloc]init];
+                card.rank = rank;
+                card.suit = suit;
+                [self addCard:card atTop:YES];
+            }
+        }
+    }
+    */
+    return self;
+}
+
+
 @end
