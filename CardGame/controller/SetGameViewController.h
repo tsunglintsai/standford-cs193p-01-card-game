17e46a752a829b90e861da8ca5531a0ef5b74018
tsunglintsai <tsunglin.tsai@gmail.com>
2013-02-08 18:19:46 -0800

    Added Basic UI

---
+++ CardGame/controller/SetGameViewController.h
@@ -0,0 +1,13 @@
+//
+//  SetGameViewController.h
+//  CardGame
+//
+//  Created by Daniela on 2/8/13.
+//  Copyright (c) 2013 Henry. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SetGameViewController : UIViewController
+
+@end
---
+++ CardGame/controller/SetGameViewController.m
@@ -0,0 +1,21 @@
+//
+//  SetGameViewController.m
+//  CardGame
+//
+//  Created by Daniela on 2/8/13.
+//  Copyright (c) 2013 Henry. All rights reserved.
+//
+
+#import "SetGameViewController.h"
+
+@interface SetGameViewController ()
+@property (strong, nonatomic) IBOutletCollection(UIButton) NSArray *cardButtons;
+
+@end
+
+@implementation SetGameViewController
+
+
+- (IBAction)flipCard:(UIButton *)sender {
+}
+@end
